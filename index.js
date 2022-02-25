a596e696faea083619007305b44e595c126d563c
Rajasekar <rajasekarb2917@gmail.com>
2022-02-25 08:44:57 +0530

    Completely updated JavaScript

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-var css = document.querySelector('h4');
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
-var body = document.getElementById('gradient');
-var randomBut = document.querySelector('.random');
+var input1 = document.querySelector('.inp1');
+var input2 = document.querySelector('.inp2');
+var h3 = document.querySelector('h3');
+var body = document.querySelector('.back');
+var randomBtn = document.querySelector('.random');
 
-function changeBackground() {
-    body.style.background = 'linear-gradient(to right, ' + color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background + ";" 
+function randomNumber() {
+    return String(parseInt(Math.random() * 256));
 }
 
-function getRndInteger() {
-    var min = 000000000;
-    var max = 999999999;
-    var range1 = Math.floor(Math.random() * (max - min + 1) ) + min;
-    var range2 = Math.floor(Math.random() * (max - min + 1) ) + min;
-    var res1 = range1 + '';
-    var res2 = range2 + '';
-    body.style.background = 'linear-gradient(to right, ' + 'rgb('+res1[0]+res1[1]+res1[2]+","+res1[3]+res1[4]+res1[5]+","+res1[6]+res1[7]+res1[8]+")" + ",rgb("+res2[0]+res2[1]+res2[2]+","+res2[3]+res2[4]+res2[5]+","+res2[6]+res2[7]+res2[8]+"))";
-    css.textContent = body.style.background + ";" 
-  }
 
-color1.addEventListener('input',changeBackground);
-color2.addEventListener('input',changeBackground);
-randomBut.addEventListener('click',getRndInteger);
+function randomColor() {
+    body.style.background = 'linear-gradient(to right, rgb(' + randomNumber() + ',' + randomNumber() + ',' + randomNumber() + '), rgb(' + randomNumber() + ',' + randomNumber() + ',' + randomNumber() + '))';
+    h3.textContent = 'linear-gradient(to right, rgb(' + randomNumber() + ',' + randomNumber() + ',' + randomNumber() + '), rgb(' + randomNumber() + ',' + randomNumber() + ',' + randomNumber() + '))' + ';';
+    
+}
+
+function changeBackground() {
+    body.style.background = 'linear-gradient(to right,'+ input1.value + ',' + input2.value,');'
+    h3.textContent = 'linear-gradient(to right,'+ input1.value + ',' + input2.value,');'
+}
+
+input1.addEventListener('input',changeBackground);
+input2.addEventListener('input',changeBackground);
+randomBtn.addEventListener('click',randomColor);
+
